6dec30d8e9432d8cebec3257f74c33f791800865
Juan David Conde Martínez <davidconde1309@gmail.com>
2025-02-15 09:54:32 -0500

    refactor: change of mappings in notification and publication

--- src/main/java/com/verdolaganation/Verdolaga_Nation/Notification/NotificationController.java
+++ src/main/java/com/verdolaganation/Verdolaga_Nation/Notification/NotificationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/notifications")
+@RequestMapping("/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
 
--- src/main/java/com/verdolaganation/Verdolaga_Nation/Publication/PublicationController.java
+++ src/main/java/com/verdolaganation/Verdolaga_Nation/Publication/PublicationController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/publications")
+@RequestMapping("/publications")
 public class PublicationController {
     private final PublicationService publicationService;
 
